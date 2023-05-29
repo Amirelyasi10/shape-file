2c24a3c7800c6df77a8d98ab4d8e413fc0e49484
Amir <Amir_eli1230@gmail.com>
2023-05-29 11:28:42 +0330

    Update file data

--- first.py
+++
@@ -1,5 +0,0 @@
-number = 5
-
-for num in range(1, number + 1):
-    shape = num * "*"
-    print(shape) 
--- second.py
+++
@@ -1,8 +0,0 @@
-from colorama import Fore
-
-number = 5
-start = 1
-
-for num in range(number, start -1, -1):
-    shape = num * "*"
-    print(Fore.RED, shape, Fore.RESET)
